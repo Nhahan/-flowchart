762fa6be6f2868e2a9a9e024ba50bdef11ea22b0
Nhahan <kisy324@naver.com>
2024-11-20 16:22:22 +0900

    feat: vite single build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
