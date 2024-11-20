f02127a15a5684988f030c5e5e0d27fbafadb7a4
Nhahan <kisy324@naver.com>
2024-11-20 12:58:45 +0900

    feat: basic flow chart

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import {FlowChartContent} from "@/features/flowchart/FlowChartContent.tsx";
+import {ReactFlowProvider} from "react-flow-renderer";
+
+const App = () => {
+	return (
+		<ReactFlowProvider>
+			<FlowChartContent />
+		</ReactFlowProvider>
+	);
+};
+
+export default App;
